b41076837727c7a0cd65d4c3460d2ebcaba86d71
greatitman <agiletiger@qq.com>
2015-11-03 17:34:35 +0800

    file operator

---
+++ test/lib_misc.erl
@@ -0,0 +1,23 @@
+-module(lib_misc).
+-export([consult/1,unconsult/2]).
+
+consult(File)	->
+	case file:open(File,read) of
+		{ok,S}	->
+			Val = consult1(S),
+			file:close(S),
+			{ok,Val};
+		{error,Why}	->
+			{error,Why}
+	end.
+consult1(S)	->
+	case io:read(S,'') of
+		{ok,Term}	-> [Term|consult1(S)];
+		eof	->	[];
+		Error	->Error
+	end.
+
+unconsult(File,L)	->
+	{ok,S} = file:open(File,write),
+	lists:foreach(fun(X)	-> io:format(S,"~p.~n",[X]) end, L),
+	file:close(S).
