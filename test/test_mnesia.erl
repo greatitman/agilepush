8940b3f091d424063423ce4d87c66a9c40756563
greatitman <agiletiger@qq.com>
2015-11-05 16:16:02 +0800

    test amnesia

---
+++ test/test_mnesia.erl
@@ -0,0 +1,14 @@
+-module(test_mnesia).
+-export([do_this_once/0]).
+
+-record(shop,{item,quantity,cost}).
+-record(cost,{name,price}).
+-record(design, {id,plan}).
+
+do_this_once()	->
+	mnesia:create_schema([node()]),
+	mnesia:start(),
+	mnesia:create_table(shop,[{attributes,record_info(fields,shop)}]),
+	mnesia:create_table(cost,[{attributes,record_info(fields,cost)}]),
+	mnesia:create_table(design,[{attributes,record_info(fields,design)}]),
+	mnesia:stop().
